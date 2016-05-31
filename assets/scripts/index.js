1de68a6707a4328e95b24e1f7693e2503e80d9b3
shkherad <shkherad@bu.edu>
2016-05-31 15:34:43 -0400

    boot strap class demo work
    create mobile first (bootstrap), modal, sass

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,12 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+
+$('#launch-modal').on('click', function () {
+  $('#sign-in-modal').modal('show')
+})
+
+$('sign-in-form').on('submit', function () {
+  $('#sign-in-modal').modal('hide')
+})
